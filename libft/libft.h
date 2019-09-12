94ded3b8a8377e49107efe97babc8cdc27ddd5fd
Uhoris Heirloom <uheirloo@ox-l3.21-school.ru>
2019-09-12 18:29:44 +0300

    add memchr, memcpy; mod libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: uheirloo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/10 11:28:47 by uheirloo          #+#    #+#             */
-/*   Updated: 2019/09/10 18:19:05 by uheirloo         ###   ########.fr       */
+/*   Created: 2019/09/12 18:08:13 by uheirloo          #+#    #+#             */
+/*   Updated: 2019/09/12 18:28:40 by uheirloo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
+void	*ft_memchr(void const *s, int c, size_t n);
+void	*ft_memcpy(void *dst, void const *src, size_t n)
 char	*ft_strdup(char const *s);
 char	*ft_strcpy(char *dest, char const *src);
 char	*ft_strncpy(char *dest, char const *src, size_t n);
@@ -26,6 +28,8 @@ char	*ft_strcat(char *s1, char const *s2);
 char	*ft_strncat(char *s1, char const *s2, size_t n);
 char	*ft_strchr(char const *s, int c);
 char	*ft_strrchr(char const *s, int c);
+char	*ft_strnstr(char const *haystack, char const *needle, size_t len);
+char	*ft_strstr(char const *haystack, char const *needle);
 int		ft_strcmp(char const *s1, char const *s2);
 int		ft_strncmp(char const *s1, char const *s2, size_t n);
 int		ft_isalpha(int c);
@@ -33,6 +37,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_isspace(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 size_t	ft_strlen(char const *s);
