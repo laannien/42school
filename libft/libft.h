78d0318259100417c1e19470e7ada69812ab1a45
Uhoris Heirloom <uheirloo@ox-l3.21-school.ru>
2019-09-13 17:59:09 +0300

    add striter

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: uheirloo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 18:08:13 by uheirloo          #+#    #+#             */
-/*   Updated: 2019/09/13 16:47:36 by uheirloo         ###   ########.fr       */
+/*   Updated: 2019/09/13 17:57:10 by uheirloo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr(int n);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_bzero(void *s, size_t n);
+void	ft_striter(char *s, void (*f)(char *));
 void	*ft_memchr(void const *s, int c, size_t n);
 void	*ft_memcpy(void *dst, void const *src, size_t n);
 void	*ft_memccpy(void *dst, void const *src, int c, size_t n);
