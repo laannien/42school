a5dc176444b0721344e885df4620b8b0c8f8ec39
laannien <laan_nien@mail.ru>
2019-09-15 14:18:26 +0300

    add strtrim

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uheirloo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/14 11:21:43 by uheirloo          #+#    #+#             */
+/*   Updated: 2019/09/14 12:56:59 by uheirloo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strtrim(char const *s)
+{
+    size_t  start;
+    size_t  end;
+    size_t  size;
+    char    *result;
+
+    if (s != NULL)
+    {
+        start = 0;
+        while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
+        {
+            start++;
+        }
+        end = ft_strlen(s);
+        while (end != start && (s[end - 1] == ' ' || s[end - 1] == '\n' || s[end - 1] == '\t'))
+        {
+            end--;
+        }
+        size = end - start;
+        result = (char*)malloc(sizeof(*s) * (size + 1));
+        if (result != NULL)
+        {
+            result = ft_strncpy(result, &s[start], size);
+            result[size] = '\0';
+        }
+        return (result);
+    }
+    return (NULL);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -51,6 +51,7 @@ char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s);
 char	*ft_itoa(int n);
 int		ft_atoi(char const *str);
 int		ft_strcmp(char const *s1, char const *s2);
