0d21e1bf7530af15ad5d7ff294d7efa339cc580b
Uhoris Heirloom <uheirloo@ox-l3.21-school.ru>
2019-09-24 16:53:36 +0300

    fix mistakes (not leaks =( )

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,56 +6,44 @@
 /*   By: uheirloo <uheirloo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 16:02:13 by uheirloo          #+#    #+#             */
-/*   Updated: 2019/09/23 16:42:23 by uheirloo         ###   ########.fr       */
+/*   Updated: 2019/09/24 16:49:08 by uheirloo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static void		ft_get_line(char *content, int length, char **line)
+char		*ft_get_line(char *content, char **line)
 {
 	char	*tmp;
 	int		i;
 
 	i = 0;
+	tmp = NULL;
 	while (content[i] != '\n' && content[i] != '\0')
 		i++;
 	if (content[i] == '\0')
 	{
 		*line = content;
-		ft_strdel(&(*content));
+		/*ft_strdel(&content);*/
+		return (NULL);
 	}
 	else
 	{
 		tmp = ft_strsub(content, 0, i);
 		*line = tmp;
 		tmp = ft_strdup(content + i + 1);
-		ft_strdel(&(*content));
+		/*ft_strdel(&content);*/
 		content = tmp;
-		free(tmp);
+		return (content);
 	}
 }
 
-static t_list	*ft_get_current(t_list **begin, const int fd)
+t_line	*ft_new_elem(int fd)
 {
-	while (*begin)
-	{
-		if ((*begin)->fd == fd)
-		{
-			return (*begin);
-		}
-		*begin = (*begin)->next;
-	}
-	(*begin)->next = new_elem(fd);
-	return ((*begin)->next);
-}
+	t_line	*new;
 
-static t_list	*new_elem(int fd)
-{
-	t_list	*new;
-
-	new = (t_list*)malloc(sizeof(t_list));
+	new = (t_line*)malloc(sizeof(t_line));
 	if (!new)
 	{
 		return (NULL);
@@ -74,18 +62,36 @@ static t_list	*new_elem(int fd)
 	return (new);
 }
 
+t_line	*ft_get_current(t_line *begin, const int fd)
+{
+	while (begin->next)
+	{
+		if (begin->fd == fd)
+		{
+			return (begin);
+		}
+		begin = begin->next;
+	}
+	if (begin->fd == fd)
+	{
+		return (begin);
+	}
+	begin->next = ft_new_elem(fd);
+	return (begin->next);
+}
+
 int				get_next_line(const int fd, char **line)
 {
-	static t_list	**begin;
-	t_list			*current;
+	static t_line	*begin;
+	t_line			*current;
 	char			buffer[BUFF_SIZE + 1];
 	char			*tmp;
 	int				length;
 
 	if (begin == NULL)
 	{
-		current = new_elem(fd);
-		begin = &current;
+		current = ft_new_elem(fd);
+		begin = current;
 	}
 	current = ft_get_current(begin, fd);
 	while ((length = read(fd, buffer, BUFF_SIZE)) > 0)
@@ -97,23 +103,42 @@ int				get_next_line(const int fd, char **line)
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
+	/*free(tmp);*/
 	if (length < 0)
 		return (-1);
 	if (length == 0 && (current->content == '\0'))
 		return (0);
-	ft_get_line(current->content, length, line);
+	current->content = ft_get_line(current->content, line);
 	return (1);
 }
 
 int				main(int argc, char **argv)
 {
-	int		fd;
+	int		fd1;
+	int		fd2;
+	int		gnl;
 	char	*result;
 
 	(void)argc;
-	fd = open(argv[1], O_RDONLY);
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_RDONLY);
 	result = NULL;
-	get_next_line(fd, &result);
-	printf("%s", result);
+	gnl = get_next_line(fd1, &result);
+	printf("%d %s\n", gnl, result);
+	get_next_line(fd2, &result);
+	printf("%s\n", result);
+	get_next_line(fd1, &result);
+	printf("%s\n", result);
+	get_next_line(fd2, &result);
+	printf("%s\n", result);
+	get_next_line(fd1, &result);
+	printf("%s\n", result);
+	get_next_line(fd2, &result);
+	printf("%s\n", result);
+	ft_strdel(&result);
+	gnl = get_next_line(fd1, &result);
+	printf("%d %s\n", gnl, result);
+	close(fd1);
+	close(fd2);
 	return (0);
 }
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,27 +6,30 @@
 /*   By: uheirloo <uheirloo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 16:02:09 by uheirloo          #+#    #+#             */
-/*   Updated: 2019/09/19 19:10:45 by uheirloo         ###   ########.fr       */
+/*   Updated: 2019/09/24 16:38:58 by uheirloo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1000000
 
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <unistd.h>
 # include <fcntl.h>
-# include "libft/libft.h"
+# include "./libft/libft.h"
 
-typedef struct	s_list
+typedef struct	s_line
 {
 	int				fd;
 	char			*content;
-	struct s_list	*next;
-}				t_list;
+	struct s_line	*next;
+}				t_line;
 
 int		get_next_line(const int fd, char **line);
+t_line	*ft_get_current(t_line *begin, const int fd);
+t_line	*ft_new_elem(int fd);
+char	*ft_get_line(char *content, char **line);
 
 #endif
