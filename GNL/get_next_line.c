f0925a6888dc9bf29b9887d032e0b99ab3a3891f
Uhoris Heirloom <uheirloo@ox-l3.21-school.ru>
2019-09-25 13:31:37 +0300

    fixed compiling, no leaks for now

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: uheirloo <uheirloo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 16:02:13 by uheirloo          #+#    #+#             */
-/*   Updated: 2019/09/24 16:49:08 by uheirloo         ###   ########.fr       */
+/*   Updated: 2019/09/25 13:21:11 by uheirloo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-char		*ft_get_line(char *content, char **line)
+char	*ft_get_line(char *content, char **line)
 {
 	char	*tmp;
 	int		i;
@@ -25,7 +24,7 @@ char		*ft_get_line(char *content, char **line)
 	if (content[i] == '\0')
 	{
 		*line = content;
-		/*ft_strdel(&content);*/
+		ft_strdel(&content);
 		return (NULL);
 	}
 	else
@@ -33,7 +32,7 @@ char		*ft_get_line(char *content, char **line)
 		tmp = ft_strsub(content, 0, i);
 		*line = tmp;
 		tmp = ft_strdup(content + i + 1);
-		/*ft_strdel(&content);*/
+		ft_strdel(&content);
 		content = tmp;
 		return (content);
 	}
@@ -80,20 +79,13 @@ t_line	*ft_get_current(t_line *begin, const int fd)
 	return (begin->next);
 }
 
-int				get_next_line(const int fd, char **line)
+int		ft_read_file(const int fd, t_line *current, char **line)
 {
-	static t_line	*begin;
-	t_line			*current;
-	char			buffer[BUFF_SIZE + 1];
-	char			*tmp;
-	int				length;
+	int		length;
+	char	buffer[BUFF_SIZE + 1];
+	char	*tmp;
 
-	if (begin == NULL)
-	{
-		current = ft_new_elem(fd);
-		begin = current;
-	}
-	current = ft_get_current(begin, fd);
+	tmp = NULL;
 	while ((length = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[length] = '\0';
@@ -103,42 +95,32 @@ int				get_next_line(const int fd, char **line)
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
-	/*free(tmp);*/
 	if (length < 0)
 		return (-1);
 	if (length == 0 && (current->content == '\0'))
 		return (0);
 	current->content = ft_get_line(current->content, line);
+	if (length == 0)
+		return (0);
 	return (1);
 }
 
-int				main(int argc, char **argv)
+int		get_next_line(const int fd, char **line)
 {
-	int		fd1;
-	int		fd2;
-	int		gnl;
-	char	*result;
+	static t_line	*begin;
+	t_line			*current;
+	int				length;
 
-	(void)argc;
-	fd1 = open(argv[1], O_RDONLY);
-	fd2 = open(argv[2], O_RDONLY);
-	result = NULL;
-	gnl = get_next_line(fd1, &result);
-	printf("%d %s\n", gnl, result);
-	get_next_line(fd2, &result);
-	printf("%s\n", result);
-	get_next_line(fd1, &result);
-	printf("%s\n", result);
-	get_next_line(fd2, &result);
-	printf("%s\n", result);
-	get_next_line(fd1, &result);
-	printf("%s\n", result);
-	get_next_line(fd2, &result);
-	printf("%s\n", result);
-	ft_strdel(&result);
-	gnl = get_next_line(fd1, &result);
-	printf("%d %s\n", gnl, result);
-	close(fd1);
-	close(fd2);
-	return (0);
+	if (begin == NULL)
+	{
+		current = ft_new_elem(fd);
+		begin = current;
+	}
+	current = ft_get_current(begin, fd);
+	length = ft_read_file(fd, current, line);
+	if (length < 0)
+		return (-1);
+	if (length == 0)
+		return (0);
+	return (1);
 }
---
+++ GNL/main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uheirloo <uheirloo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/25 12:20:16 by uheirloo          #+#    #+#             */
+/*   Updated: 2019/09/25 12:24:08 by uheirloo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int				main(int argc, char **argv)
+{
+	int		fd1;
+	int		fd2;
+	int		gnl;
+	char	*result;
+
+	(void)argc;
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_RDONLY);
+	result = NULL;
+	gnl = get_next_line(fd1, &result);
+	printf("%d %s\n", gnl, result);
+	get_next_line(fd2, &result);
+	printf("%s\n", result);
+	get_next_line(fd1, &result);
+	printf("%s\n", result);
+	get_next_line(fd2, &result);
+	printf("%s\n", result);
+	get_next_line(fd1, &result);
+	printf("%s\n", result);
+	get_next_line(fd2, &result);
+	printf("%s\n", result);
+	ft_strdel(&result);
+	gnl = get_next_line(fd1, &result);
+	printf("%d %s\n", gnl, result);
+	close(fd1);
+	close(fd2);
+	return (0);
+}
